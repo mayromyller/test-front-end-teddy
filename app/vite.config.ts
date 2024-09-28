6d628405a2f69da20f887954da56526e62fe9d89
mayromyller <deehmyller11@gmail.com>
2024-09-28 20:55:58 -0300

    chore: tailwindcss files generate and update vite config with alias path

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,31 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: "remote_a",
-      filename: "remoteEntry.js",
+      name: 'remote_a',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./App": "./src/App.tsx",
+        './App': './src/App.tsx'
       },
-      shared: ["react", "react-dom"],
+      shared: ['react', 'react-dom']
     })
   ],
   build: {
     modulePreload: false,
-    target: "esnext",
+    target: 'esnext',
     minify: false,
-    cssCodeSplit: false,
+    cssCodeSplit: false
   },
   server: {
-    port: 3001,
+    port: 3001
   },
   preview: {
     port: 3001,
     host: '0.0.0.0'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
