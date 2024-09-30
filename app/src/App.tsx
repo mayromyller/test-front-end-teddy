6c899663b9af10f26a24662defacd8e56cde05b3
mayromyller <deehmyller11@gmail.com>
2024-09-30 14:11:39 -0300

    chore: add queryClient fot tanstack and provider

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,15 @@
 import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+
 import { router } from './routes/router'
+import { queryClient } from './api/client/query-client'
 
 export default function App() {
   return (
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   )
 }
-
