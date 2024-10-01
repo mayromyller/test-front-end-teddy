b0a6520b709f619f0befe38bc016a176c7716789
mayromyller <deehmyller11@gmail.com>
2024-10-01 14:45:24 -0300

    test: testing the header component of link tag have an attribute relative a current route

--- app/src/components/app-components/__test__/header.test.tsx
+++ app/src/components/app-components/__test__/header.test.tsx
@@ -1,5 +1,34 @@
+import { render } from '@/test/test-utils'
+import { Header } from '../header'
+
 describe('Header component', () => {
-  it('should render the first test', () => {
-    expect(1 + 1).toBe(2)
+  it('should render the clients link with the active attribute', () => {
+    const screen = render(<Header />, {}, ['/clients'])
+
+    const clients = screen.getByRole('link', { name: 'Clientes' }).dataset
+      .active
+    const activeClients = screen.getByRole('link', {
+      name: 'Clientes selecionados'
+    }).dataset.active
+    const exit = screen.getByRole('link', { name: 'Sair' }).dataset.active
+
+    expect(clients).toEqual('true')
+    expect(activeClients).toEqual('false')
+    expect(exit).toEqual('false')
+  })
+
+  it('should render the selected clients link with the active attribute', () => {
+    const screen = render(<Header />, {}, ['/selected-clients'])
+
+    const clients = screen.getByRole('link', { name: 'Clientes' }).dataset
+      .active
+    const activeClients = screen.getByRole('link', {
+      name: 'Clientes selecionados'
+    }).dataset.active
+    const exit = screen.getByRole('link', { name: 'Sair' }).dataset.active
+
+    expect(activeClients).toEqual('true')
+    expect(clients).toEqual('false')
+    expect(exit).toEqual('false')
   })
 })
