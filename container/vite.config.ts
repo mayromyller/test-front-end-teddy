9aef4896849955653001e4d2e14882ee60d10f1b
mayromyller <deehmyller11@gmail.com>
2024-10-01 17:46:22 -0300

    chore: add proxy in container app

--- container/vite.config.ts
+++ container/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
       remotes: {
         remote: "http://localhost:3001/assets/remoteEntry.js",
       },
-      shared: ["react", "react-dom"]
+      shared: ["react", "react-dom"],
     }),
   ],
   build: {
@@ -25,9 +25,16 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     host: true,
-    cors: true
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "https://boasorte.teddybackoffice.com.br/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   preview: {
     port: 3000,
-  }
-})
+  },
+});
